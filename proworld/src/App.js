b717c136551bfe84ddd397e36019b6d3a4350bb8
talktorohini21@gmail.com <talktorohini21@gmail.com>
2021-06-07 13:38:32 +0530

    react

---
+++ proworld/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Front1 from "./components/front/front1";
+import About from "./components/about1/about";
+export default function App() {
+  return (
+    <div>
+      <Front1 />
+      <About />
+    </div>
+  );
+}
